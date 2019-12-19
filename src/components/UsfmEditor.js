d20122381c4964351407076f9c8d96d666d79c7d
KJoslyn <KJoslyn29@gmail.com>
2019-12-19 15:27:01 -0500

    Ability to remove section header by clicking section header button

--- src/components/UsfmEditor.js
+++ src/components/UsfmEditor.js
@@ -70,8 +70,8 @@ class UsfmEditor extends React.Component {
         const { fragment, selection } = value
 
         if (selection.isBlurred || selection.isCollapsed || fragment.text === '') {
-        menu.removeAttribute('style')
-        return
+            // menu.removeAttribute('style')
+            return
         }
 
         const native = window.getSelection()
