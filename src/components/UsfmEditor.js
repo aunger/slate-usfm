50ba799261d45c68c7ab2e90a4666685d4fafc1f
KJoslyn <KJoslyn29@gmail.com>
2020-06-08 11:53:52 -0400

    Using HOC for verse number menu

--- src/components/UsfmEditor.js
+++ src/components/UsfmEditor.js
@@ -10,6 +10,7 @@ import { NodeTypes } from "../utils/NodeTypes";
 import { HoveringToolbar } from "./HoveringToolbar";
 import { slateToUsfm } from "../transforms/slateToUsfm";
 import { debounce } from "debounce";
+import { compose } from "../utils/commonFunctions"
 
 /**
  * A WYSIWYG editor component for USFM
@@ -79,6 +80,3 @@ export const UsfmEditor = ({
         </Slate>
     )
 }
-
-const compose = (...functions) =>
-    args => functions.reduceRight((arg, fn) => fn(arg), args)
