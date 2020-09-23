1bb7be90893a74b37fe7b6661957445776201925
KJoslyn <KJoslyn29@gmail.com>
2020-09-23 19:08:44 -0400

    UsfmEditor now a class to be extended. Rename old UsfmEditor to BasicUsfmEditor.

--- src/components/UsfmEditor.js
+++ src/components/UsfmEditor.js
@@ -1,133 +1,65 @@
 import * as React from "react";
-import { withReact, Slate, Editable } from "slate-react";
-import { createEditor, Transforms } from 'slate';
-import { renderElementByType, renderLeafByProps } from '../transforms/usfmRenderer';
-import { usfmToSlate } from '../transforms/usfmToSlate';
-import { withNormalize } from "../plugins/normalizeNode";
-import { handleKeyPress, withBackspace, withDelete, withEnter } from '../plugins/keyHandlers';
-import NodeTypes from "../utils/NodeTypes";
-import { HoveringToolbar } from "./HoveringToolbar";
-import { slateToUsfm } from "../transforms/slateToUsfm";
-import { debounce } from "debounce";
-import { flowRight, isEqual } from "lodash"
-import { MyTransforms } from "../plugins/helpers/MyTransforms";
-import { parseIdentificationFromUsfm, 
-         filterInvalidIdentification,
-         mergeIdentification,
-         normalizeIdentificationValues
-} from "../transforms/identificationTransforms";
-import { MyEditor } from "../plugins/helpers/MyEditor";
-import { PropTypes } from "prop-types" 
-import "./default.css";
+import { Editor, Transforms } from "slate";
+import { UsfmMarkers } from "../utils/UsfmMarkers";
+import NodeRules from "../utils/NodeRules";
 
 /**
- * A WYSIWYG editor component for USFM
+ * The UsfmEditor class implements the USFM Editor API.
+ * The API methods can be called on any UsfmEditor, regardless of where
+ * the editor is in the editor hierarchy.
+ * 
+ * This class provides a react ref that must be passed into the 'ref' prop of a
+ * child UsfmEditor component. The ref provides access to the lowest-level
+ * editor via the baseEditor() function. However, it should not be necessary to call 
+ * the baseEditor() function directly. It is used by the API functions.
  */
 export class UsfmEditor extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
-            value: usfmToSlate(props.usfmString) 
-        }
-        this.editor = flowRight(
-            withBackspace,
-            withDelete,
-            withEnter,
-            withNormalize,
-            withReact,
-            createEditor
-        )()
-        this.editor.isInline = element => {
-            return false
+
+        /* Pass this into the 'ref' prop of a child UsfmEditor component */
+        this.childEditorRef = React.createRef()
+
+        /* Returns the base (lowest-level) editor. This is the slate editor. */
+        this.baseEditor = () => this.childEditorRef.current.baseEditor()
+
+        this.getMarksAtCursor = () => {
+            if (!this.childEditorRef.current || !this.baseEditor().selection) return []
+            return Editor.marks(this.baseEditor())
         }
 
-        this.handleChange = value => {
-            console.debug("after change", value)
-            if (MyEditor.isVerseOrChapterNumberSelected(this.editor)) {
-                Transforms.deselect(this.editor)
-                return
-            }
-            this.setState({ value: value })
-            this.scheduleOnChange(value)
+        this.addMarkAtCursor = (mark) => {
+            if (!this.baseEditor().selection) return
+            Editor.addMark(this.baseEditor(), mark, true)
         }
 
-        this.scheduleOnChange = debounce(function(newValue) {
-            const usfm = slateToUsfm(newValue)
-            this.props.onChange(usfm)
-        }, 200)
-
-        this.onKeyDown = event => {
-            handleKeyPress(event, this.editor)
+        this.removeMarkAtCursor = (mark) => {
+            if (!this.baseEditor().selection) return
+            Editor.removeMark(this.baseEditor(), mark)
         }
 
-        this.updateIdentificationFromProp = () => {
-            const current = MyEditor.identification(this.editor)
-            const validUpdates = this.filterAndNormalize(this.props.identification)
-            const updated = mergeIdentification(current, validUpdates)
-
-            if (! isEqual(updated, current)) {
-                MyTransforms.setIdentification(this.editor, updated)
-                if (this.props.onIdentificationChange) {
-                    this.props.onIdentificationChange(updated)
+        this.getParagraphTypesAtCursor = () => {
+            if (!this.childEditorRef.current || !this.baseEditor().selection) return []
+            let types = []
+            const nodes = Editor.nodes(this.baseEditor())
+            let entry = nodes.next()
+            while (!entry.done) {
+                const node = entry.value[0]
+                if (UsfmMarkers.isParagraphType(node)) {
+                    types = types.concat(node.type)
                 }
+                entry = nodes.next()
             }
+            return types
         }
 
-        this.updateIdentificationFromUsfmAndProp = () => {
-            const parsedIdentification = parseIdentificationFromUsfm(this.props.usfmString)
-            const validParsed = this.filterAndNormalize(parsedIdentification)
-            const validUpdates = this.filterAndNormalize(this.props.identification)
-            const updated = mergeIdentification(validParsed, validUpdates)
-
-            MyTransforms.setIdentification(this.editor, updated)
-            if (this.props.onIdentificationChange) {
-                this.props.onIdentificationChange(updated)
-            }
-        }
-
-        this.filterAndNormalize = (idJson) => {
-            const filtered = filterInvalidIdentification(idJson)
-            return normalizeIdentificationValues(filtered)
+        this.setParagraphTypeAtCursor = (marker) => {
+            if (!this.baseEditor().selection) return
+            Transforms.setNodes(
+                this.baseEditor(),
+                { type: marker },
+                { match: NodeRules.isFormattableBlockType }
+            )
         }
     }
-
-    componentDidMount() {
-        this.updateIdentificationFromUsfmAndProp()
-    }
-    
-    componentDidUpdate(prevProps) {
-        if (prevProps.usfmString != this.props.usfmString) {
-            this.updateIdentificationFromUsfmAndProp()
-        } else if (prevProps.identification != this.props.identification) {
-            this.updateIdentificationFromProp()
-        }
-    }
-
-    render() {
-        return (
-            <Slate
-                editor={this.editor}
-                value={this.state.value}
-                onChange={this.handleChange}
-            >
-                <HoveringToolbar />
-                <Editable
-                    readOnly={this.props.readOnly}
-                    renderElement={renderElementByType}
-                    renderLeaf={renderLeafByProps}
-                    spellCheck={false}
-                    onKeyDown={this.onKeyDown}
-                    className={"usfm-editor"}
-                />
-            </Slate>
-        )
-    }
-}
-
-UsfmEditor.propTypes = {
-    usfmString: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-    readOnly: PropTypes.bool.isRequired,
-    identification: PropTypes.object,
-    onIdentificationChange: PropTypes.func
 }
--- src/demo/EditorDemo.js
+++ src/demo/EditorDemo.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { UsfmEditor } from "../components/UsfmEditor";
+import { BasicUsfmEditor } from "../components/BasicUsfmEditor";
 import { InputSelector } from "./InputSelector";
 import { usfmToSlate } from "../transforms/usfmToSlate.js";
 import { slateToUsfm } from "../transforms/slateToUsfm.ts";
@@ -7,6 +7,7 @@ import { OptionCheckbox } from "./OptionCheckbox";
 import { InputUsfm, OutputUsfm } from "./UsfmContainer";
 import { IdentificationSetter } from "./IdentificationSetter";
 import "./demo.css";
+import { ToolbarEditor } from "./ToolbarEditor";
 
 function transformToOutput(usfm) {
     return slateToUsfm(usfmToSlate(usfm))
@@ -86,7 +87,7 @@ export class EditorDemo extends React.Component {
                             idJson={JSON.stringify(this.state.identification)} 
                             onChange={this.onIdentificationChange} />
                         <h2>Editor</h2>
-                        <UsfmEditor
+                        <ToolbarEditor
                             usfmString={this.state.usfmInput}
                             key={this.state.usfmInput}
                             onChange={this.handleEditorChange}
