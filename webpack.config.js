f37febe3c3ef1d76885961b5244421b7750c0263
KJoslyn <KJoslyn29@gmail.com>
2021-05-10 11:18:24 -0400

    Use file-loader instead of url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
             },
             {
                 test: /\.svg$/,
-                use: ["@svgr/webpack", "url-loader"],
+                use: ["@svgr/webpack", "file-loader"],
             },
         ],
     },
